bf955312e5fab20528294d0a68966e42431fa18d
Giulio Ermanno Pibiri <jeis90@gmail.com>
2023-10-07 17:10:54 +0200

    support batch mode

--- tools/cluster.cpp
+++ tools/cluster.cpp
@@ -14,12 +14,15 @@ int main(int argc, char** argv) {
     parser.add("max_iter", "Number of maximum iterations.", "-m", false);
     parser.add("min_delta", "Minimum difference in means.", "-d", false);
     parser.add("seed", "Random seed for kmeans++ initialization.", "-s", false);
+    parser.add("batch_size",
+               "Use batch mode: compute k clusters for batches of batch_size vectors.", "-b",
+               false);
 
     if (!parser.parse()) return 1;
 
     auto k = parser.get<uint64_t>("k");
     if (k > (uint64_t(1) << 32)) {
-        std::cout << "Error: number of clusters cannot be more than 2^32." << std::endl;
+        std::cerr << "Error: number of clusters cannot be more than 2^32." << std::endl;
         return 1;
     }
 
@@ -28,31 +31,46 @@ int main(int argc, char** argv) {
     if (parser.parsed("min_delta")) {
         float_type d = parser.get<float_type>("min_delta");
         if (d < 0) {
-            std::cout << "Error: min_delta cannot be < 0" << std::endl;
+            std::cerr << "Error: min_delta cannot be < 0" << std::endl;
             return 1;
         }
         params.set_min_delta(d);
     }
     if (parser.parsed("seed")) params.set_random_seed(parser.get<uint64_t>("seed"));
 
-    std::vector<point> points;
-
-    {
-        std::ifstream in(parser.get<std::string>("byte_vectors_filename"), std::ios::binary);
-        uint64_t num_bytes_per_point = 0;
-        uint64_t num_points = 0;
-        in.read(reinterpret_cast<char*>(&num_bytes_per_point), sizeof(uint64_t));
-        in.read(reinterpret_cast<char*>(&num_points), sizeof(uint64_t));
-        points.resize(num_points, point(num_bytes_per_point));
-        for (uint64_t i = 0; i != num_points; ++i) {
-            in.read(reinterpret_cast<char*>(points[i].data()), num_bytes_per_point);
+    uint64_t batch_size = 0;
+    if (parser.parsed("batch_size")) {
+        batch_size = parser.get<uint64_t>("batch_size");
+        if (batch_size == 0) {
+            std::cerr << "Error: batch_size cannot be 0" << std::endl;
+            return 1;
         }
-        in.close();
     }
 
-    auto labels = kmeans_lloyd(points, params);
+    std::vector<point> points;
 
-    for (auto l : labels) std::cout << l << " ";
+    std::ifstream in(parser.get<std::string>("byte_vectors_filename"), std::ios::binary);
+    uint64_t num_bytes_per_point = 0;
+    uint64_t num_points = 0;
+    in.read(reinterpret_cast<char*>(&num_bytes_per_point), sizeof(uint64_t));
+    in.read(reinterpret_cast<char*>(&num_points), sizeof(uint64_t));
+
+    if (batch_size == 0) batch_size = num_points;
+
+    for (uint64_t batch = 0; num_points != 0; ++batch) {
+        batch_size = std::min<uint64_t>(batch_size, num_points);
+        points.resize(batch_size, point(num_bytes_per_point));
+        for (uint64_t i = 0; i != batch_size; ++i) {
+            in.read(reinterpret_cast<char*>(points[i].data()), num_bytes_per_point);
+        }
+        std::cerr << "batch-" << batch << ": running kmeans with k = " << k << " for "
+                  << points.size() << " points" << std::endl;
+        auto labels = kmeans_lloyd(points, params);
+        for (auto l : labels) std::cout << l << " ";
+        num_points -= batch_size;
+    }
+
+    in.close();
 
     return 0;
 }
